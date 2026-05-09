0f4a6c70ce06527256317d43e1e070d16a511e25
Monzo-2003 <m0hu55ain2072@gmail.com>
2026-05-09 16:40:49 +0100

    Initial program skeleton running perfectly

---
+++ io.h
@@ -0,0 +1,6 @@
+#ifndef IO_H
+#define IO_H
+
+#include "waveform.h"
+
+#endif
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Power Quality Waveform Analyser\n");
+    return 0;
+}
